b82379d29356e0b2187a8de46c6c6456e7a29943
SebasRamirez97 <ramirez.sebas97@gmail.com>
2026-09-21 15:46:26 -0300

    app.ts
    Refactor App component to implement OnInit and manage user roles.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.css',
   templateUrl: './app.html',
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('sebastian-ramirezTP1');
+export class AppComponent implements OnInit {
+  rol: string = '';
+
+  constructor(private authService: AuthService) {}
+
+  async ngOnInit() {
+    // 1. Intentar obtener cliente registrado
+    const user = await this.authService.getUser();
+    if (user) {
+      this.rol = user.user_metadata?.rol || '';
+      return;
+    }
+
+    // 2. Intentar obtener cliente anónimo
+    const anonimo = this.authService.getAnonimo();
+    if (anonimo) {
+      this.rol = anonimo.rol;
+      return;
+    }
+
+    // 3. Si no hay sesión activa
+    this.rol = '';
+  }
 }
